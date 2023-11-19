d72a7fc880743f5f6d982459d7201861cc054cf4
PrinceStiles <princekarpture@gmail.com>
2023-11-19 18:15:40 +0000

    tbc

---
+++ Basic 01/script.js
@@ -0,0 +1,15 @@
+// 1. Create varaible name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally, log all variables to the console.
+
+const name = "PrinceStiles";
+const whatDoYouWannaBecomeInYourLife = "programmer";
+const gender = "male";
+const twitterHandle = "@PrinceStiles";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
