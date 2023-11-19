9f7ec5d4b21195338c4bd7394159121de0132763
PrinceStiles <princekarpture@gmail.com>
2023-11-19 18:53:06 +0000

    tbc_js

--- Basic 01/script.js
+++ Basic 01/script.js
@@ -13,3 +13,32 @@ console.log(name);
 console.log(whatDoYouWannaBecomeInYourLife);
 console.log(gender);
 console.log(twitterHandle);
+
+// 1. Create variable name (firstFavNum) & store your favorite number.
+// 2. Create variable name (secondFavBum) & store your fav number
+// 3. Add (firstFavNum & secondFavNum).
+// 4. Subtract (firstFavNum & secondFavNum).
+// 5. Multiply (firstFavNum & secondFavNum).
+// 6. Divide (firstFavNum & secondFavNum).
+// 7. check (firstFavNum Mod secondFavNum).
+// 8. check the power of (firstFavNum).
+
+const firstFavNum = 17;
+const secondFavBum = 5;
+console.log(firstFavNum + secondFavBum);
+console.log(firstFavNum - secondFavBum);
+console.log(firstFavNum * secondFavBum);
+console.log(firstFavNum / secondFavBum);
+console.log(firstFavNum & secondFavBum);
+console.log(firstFavNum ** firstFavNum);
+
+// 1. Create variable name (isJsProgrammingLanguage) & store true as a value
+// 2. Create variable name (isJsHard) & store false as a value
+// 3. Create variable name (favNumb) & store your favorite number
+// 4. Now add fovNumb with the value of undefined.
+
+const isJsProgrammingLanguage = true;
+const isJsHard = false;
+const favNumb = 13;
+
+console.log(favNumb + undefined);
